27ed9336dd8672eae0aae733a5342c0ca4ce4e37
yeknock <yen.martirosyan@gmail.com>
2024-11-24 23:09:57 +0400

    added parsing.h, enumeration, a struct for commands linked list and a struct for current state

--- includes/parsing.h
+++ includes/parsing.h
@@ -2,13 +2,25 @@
 # define PARSING_H
 
 
-typedef enum e_parse_state
+typedef enum e_parser_state
 {
-	EXPECT_COMMAND,
-	EXPECT_ARGUMENT,
-	EXPECT_FILE,
-	ERROR_STATE
-} t_parse_state;
+    EXPECT_COMMAND,       // Expecting a command (after a pipe, or at the beginning).
+    EXPECT_ARGUMENT,      // Expecting arguments (after a command or redirection).
+    EXPECT_PIPE,          // Expecting a pipe symbol.
+    EXPECT_REDIRECT_IN,   // Expecting an input redirection ("<").
+    EXPECT_REDIRECT_OUT,  // Expecting an output redirection (">").
+    EXPECT_REDIRECT_APPEND, // Expecting a redirection with append (">>").
+    EXPECT_HEREDOC,       		// Expecting a heredoc ("<<").
+    ERROR_STATE           		// Syntax error state.
+}   t_parser_state;
+
+
+typedef struct s_parser_state {
+    t_token_type current_type;   // Type of the current token (e.g., COMMAND, ARGUMENT)
+    t_token *current_token;      // Pointer to the current token being processed
+    int index;                   // Current position in the input string (or token list)
+    t_token **tokens_list;       // List of tokens to be parsed
+} t_parser_state;
 
 
 typedef struct s_command {
